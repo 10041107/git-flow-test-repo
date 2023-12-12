53c100e115825c5e065ff8a8ddd7e87a925c9ee4
gorilla <gorilla.ohgiraffers@gmail.com>
2023-12-12 09:38:31 +0900

    first commit

---
+++ src/test/java/com/ohgiraffers/gitflowtest/GitFlowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gitflowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
